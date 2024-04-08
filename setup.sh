54911e5905eaff06ba210aece4c9f6ebf2466803
KendleMintJed <jamie.astonfamily@gmail.com>
2024-04-08 02:08:44 +0100

    Bug fixes. Git config. Zsh config. Fixed bug where nvm wouldn't install Fixed bug where shell wouldn't switch properly User is now asked to enter essential git config info Zsh config file is now downloaded

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cd
 touch ~/.hushlogin
+
+# Get git credentials
+read -p "Enter git username: " gitUser
+read -p "Enter git email: " gitEmail
 
 # Update packages
 sudo apt update -y
@@ -10,9 +13,13 @@ sudo apt upgrade -y
 # Install dependencies
 sudo apt install git zsh build-essential luarocks cargo dotnet6 ripgrep -y
 
+# Set git config
+git config --global user.name "$gitUser"
+git config --global user.email $gitEmail
+
 # Install Node.js
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-use exec bash
+. ~/.bashrc
 nvm install 21
 
 # Install NeoVim
@@ -22,7 +29,8 @@ git clone https://github.com/NvChad/starter ~/.config/nvim
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $(whoami)
+curl -o ~/.zshrc https://raw.githubusercontent.com/KendleMintJed/Ubendle/main/.zshrc
 
 # Launch NeoVim
 nvim
